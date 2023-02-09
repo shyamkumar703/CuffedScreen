a10789e0ff4bcb466f2fea0063a59634352c9a88
Shyam Kumar <shyamkumar@berkeley.edu>
2023-02-09 15:16:15 -0800

    Initial Commit

---
+++ CuffedScreen/CuffedScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuffedScreenApp.swift
+//  CuffedScreen
+//
+//  Created by Shyam Kumar on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CuffedScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
